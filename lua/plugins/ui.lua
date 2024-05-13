6dfb0713d8dd622f3b3fb371197ef85f7f99007e
zmwilliam <willcxa@gmail.com>
2024-05-13 11:45:16 -0300

    updating bufferline config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -65,4 +65,14 @@ return {
       },
     },
   },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
 }
