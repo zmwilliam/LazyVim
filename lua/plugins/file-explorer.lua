5979ea0a9de3ad48f0e8f90ca8b5801566560c8f
zmwilliam <willcxa@gmail.com>
2024-06-03 08:05:10 -0300

    feat(file-explorer): replacing neo-tree with oil.nvim

---
+++ lua/plugins/file-explorer.lua
@@ -0,0 +1,23 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      columns = {
+        "icon",
+        -- "size",
+        -- "mtime",
+      },
+      view_options = { show_hidden = true },
+      keymaps = {
+        ["q"] = "actions.close",
+        ["H"] = "actions.toggle_hidden",
+      },
+    },
+    keys = {
+      { "<leader>E", function() require("oil").toggle_float(".") end, desc = "Explorer Oil (Root dir)", remap = true },
+      { "<leader>e", function() require("oil").toggle_float() end, desc = "Explorer Oil (cwd)", remap = true },
+    },
+  },
+}
